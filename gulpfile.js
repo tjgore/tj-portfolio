b8f1036019741281c4ea610756fd48580a3a0071
TJ Gore <hello@tjwgore.com>
2019-09-29 21:45:29 -0400

    Added articles

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const maps = require('gulp-sourcemaps')
+const purgecss = require('gulp-purgecss')
 const cleanCSS = require('gulp-clean-css')
 const removeEmptyLines = require('gulp-remove-empty-lines')
 const prettyHtml = require('gulp-pretty-html')
+
+const publicHtml = ['public/*.html', 'public/**/*.html', 'public/**/**/*.html']
 
 function cssToSass() {
 return gulp.src(['assets/scss/styles.scss'])
@@ -18,7 +21,7 @@ return gulp.src(['assets/scss/styles.scss'])
 }
 
 function bootstrapSass() {
-  return gulp.src(['assets/scss/bootstrap/bootstrap.scss'])
+  return gulp.src(['assets/scss/bootstrap.scss'])
   .pipe(maps.init())
   .pipe(sass(
   {
@@ -28,6 +31,14 @@ function bootstrapSass() {
   .pipe(maps.write('./maps'))
   .pipe(gulp.dest('static/css/'))
 }
+
+gulp.task('purgecss', () => {
+  return gulp.src('public/css/*.css')
+      .pipe(purgecss({
+          content: [...publicHtml]
+      }))
+      .pipe(gulp.dest('public/css'))
+})
 
 gulp.task('build', gulp.parallel(bootstrapSass, cssToSass))
 
